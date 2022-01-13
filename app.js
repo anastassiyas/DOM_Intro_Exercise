8e9d4b52297ef1409f3cd4bf949a46d90593ef94
anastassiya.savostina <a.savostina@icloud.com>
2022-01-13 00:45:53 -0700

    done

---
+++ app.js
@@ -0,0 +1,63 @@
+console.log("Javascript is alive!");
+// In a separate JS file:
+
+// When the page loads:
+
+// Change the greeting from "Hello, There!" to "Hello, World!".
+document.getElementById("greeting").textContent = "Hello, World!";
+
+// Set the background color of each <li> to yellow.
+
+// Create an image tag, set its src attribute to http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif, and append the to the #greeting div.
+
+// Create and add a ul element to the end of the body with a class of "todo-items"
+
+// Go through the array ['make coffee','eat donut','change diapers','drive to work'] and create an li element for each item e.g. <li>make coffee</li>
+
+// Add each newly created li element to your ul of class "todo-items"
+
+// Part Two:
+// Afterwards:
+
+// Add the class of selected to an <li> when it is clicked. Remove it from any other lis as well.
+
+// Change the image to be the most recently clicked food item.
+
+// When the gray div is moused over, it's removed from the DOM.
+
+// When the orange div is moused over, its width doubles. When the mouse moves out of the div, it returns to its original size.
+
+// When the reset button is clicked - remove the selected class from each <li> and change the image to panic.jpeg.
+
+// When the 1, 2, 3, 4, 5, 6, 7, 8, 9, or 0 key is pressed, the page alerts the message "I HATE NUMBERZZZ!"
+
+// BONUS: If someone types the Konami Code, the page alerts "YOU ARE AN EVENT HANDLER GURUUUUUUUUU!"
+
+// About
+// No description, website, or topics provided.
+// Resources
+//  Readme
+// Stars
+//  0 stars
+// Watchers
+//  1 watching
+// Forks
+//  192 forks
+// Releases
+// No releases published
+// Packages
+// No packages published
+// Languages
+// HTML
+// 78.0%
+ 
+// CSS
+// 18.2%
+ 
+// JavaScript
+// 3.8%
+// © 2022 GitHub, Inc.
+// Terms
+// Privacy
+// Security
+// Status
