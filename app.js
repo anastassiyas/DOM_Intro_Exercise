250382ab39de302f8d6a20d9444538508d8af33e
anastassiya.savostina <a.savostina@icloud.com>
2022-01-13 11:18:16 -0700

    done

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ console.log("Javascript is alive!");
 // When the page loads:
 
 // Change the greeting from "Hello, There!" to "Hello, World!".
-document.getElementById("greeting").textContent = "Hello, World!";
+document.getElementById('greeting').innerHTML = 'Hello, World!';
 
 // Set the background color of each <li> to yellow.
+var list_items = document.querySelectorAll("li");
+for (var i = list_items.length - 1; i >= 0; i--) {
+list_items[i].style.backgroundColor = "yellow";
+}
 
 // Create an image tag, set its src attribute to http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif, and append the to the #greeting div.
+var newImg = document.createElement('img');
+newImg.src = 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif';
+document.getElementById('greeting').appendChild(newImg);
 
 // Create and add a ul element to the end of the body with a class of "todo-items"
+
+
 
 // Go through the array ['make coffee','eat donut','change diapers','drive to work'] and create an li element for each item e.g. <li>make coffee</li>
 
