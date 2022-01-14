424e6626a1e9f7e32b9061b784fcfbfbe979c403
anastassiya.savostina <a.savostina@icloud.com>
2022-01-14 12:40:59 -0700

    done

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 console.log("Javascript is alive!");
 // In a separate JS file:
-
 // When the page loads:
-
 // Change the greeting from "Hello, There!" to "Hello, World!".
 document.getElementById('greeting').innerHTML = 'Hello, World!';
 
@@ -19,7 +17,6 @@ newImg.src = 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif';
 document.getElementById('greeting').appendChild(newImg);
 
 // Create and add a ul element to the end of the body with a class of "todo-items"
-
 var toDoItems = document.createElement('ul')
 toDoItems.className = "todo-items";
 toDoItems.style.color = "black";
@@ -27,7 +24,6 @@ document.body.appendChild(toDoItems);
 
 
 // Go through the array ['make coffee','eat donut','change diapers','drive to work'] and create an li element for each item e.g. <li>make coffee</li>
-
 let list = ['Make Coffee', 'Eat Donut', 'Change Diapers', 'Drive To Work'];
 // Add each newly created li element to your ul of class "todo-items"
 var newLi = document.createElement('li')
@@ -76,18 +72,6 @@ var removeDiv = document.getElementById("ghosting");
 removeDiv.onmouseover = function (){
   removeDiv.remove();
 }
-// Change the image to be the most recently clicked food item.
-// var img1 = (document.getElementsByTagName("img"))
-
-// showImage = function (e) {
- 
-//   img1[1].src = "./images/images.jfif"+ e.target.innerHTML + ".jpeg"
-// }
-
-// for (var i = 0; i < elems.length; i++){
-//     elems[i].addEventListener('mousedown', showImage);
-// }
-
 
 // When the gray div is moused over, it's removed from the DOM.
 var divs = (document.querySelectorAll("div"))
@@ -98,31 +82,19 @@ divs[4].addEventListener('mouseover', grayOut);
 
 
 // When the orange div is moused over, its width doubles. When the mouse moves out of the div, it returns to its original size.
-
 var getWide = function (e) {
     divs[5].style.width = "400px";
   }
   var getNarrow = function (e) {
     divs[5].style.width = "200px";
   }
-  
-  
+    
   divs[5].addEventListener('mouseover', getWide);
   divs[5].addEventListener('mouseout', getNarrow);
   
   console.log(document.querySelectorAll('div'))
   
-// When the reset button is clicked - remove the selected class from each <li> and change the image to panic.jpeg.
-var inputButton = document.querySelector("input")
-var panic = function (e){
-  for (var i = 0; i < elems.length; i++){
-      elems[i].classList.remove('selected');
-    }
-  img1[1].src = "./images/panic.jpeg"
-}
-inputButton.addEventListener('click', panic);
-
-    
+  
 // When the 1, 2, 3, 4, 5, 6, 7, 8, 9, or 0 key is pressed, the page alerts the message "I HATE NUMBERZZZ!"
 window.onkeydown = function(event) {
     if (event.keyCode = 0-9) {
@@ -130,35 +102,35 @@ window.onkeydown = function(event) {
     }
   };
 // BONUS: If someone types the Konami Code (https://en.wikipedia.org/wiki/Konami_Code), the page alerts "YOU ARE AN EVENT HANDLER GURUUUUUUUUU!"
-var neededkeys = [38,38,40,40,37,39,37,39,66,65]
-var started = false;
-var count = 0;
-var konami = function(e) {
-    key = e.keyCode;
-    if (!started) {
-        if (key == 38) {
-            started = true;
-        }
-    }
-    if (started) {
-        if (neededkeys[count] == key) {
-            count++;
-        } else {
-            reset();
-        }
-        if (count == 10) {
-            reset();
-            // Do your stuff here
-            alert("YOU ARE AN EVENT HANDLER GURUUUUUUUUU!");
+// var neededkeys = [38,38,40,40,37,39,37,39,66,65]
+// var started = false;
+// var count = 0;
+// var konami = function(e) {
+//     key = e.keyCode;
+//     if (!started) {
+//         if (key == 38) {
+//             started = true;
+//         }
+//     }
+//     if (started) {
+//         if (neededkeys[count] == key) {
+//             count++;
+//         } else {
+//             reset();
+//         }
+//         if (count == 10) {
+//             reset();
+//             // Do your stuff here
+//             alert("YOU ARE AN EVENT HANDLER GURUUUUUUUUU!");
 
-        }
-    } else {
-        reset();
-    }
-};
-function reset() {
-    started = false;
-    count = 0;
-}
+//         }
+//     } else {
+//         reset();
+//     }
+// };
+// function reset() {
+//     started = false;
+//     count = 0;
+// }
 
-document.onkeydown = konami;
+// document.onkeydown = konami;
